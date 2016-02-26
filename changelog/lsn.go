4354c2f7cf4b0581de9a8cda7734cc44731fbac6
Gregory Brail <greg@apigee.com>
2016-02-25 21:06:13 -0600

    Remove dead code.

--- changelog/lsn.go
+++
@@ -1,9 +0,0 @@
-/*
- * Functions related to manipulating LSNs.
- * An LSN is an unsigned 64-bit quantity that identifies an extent and
- * a position within that extent.
- * Each is a 32-bit quantity, so we support 2^32 extents and a maximum
- * file size of 2^32, so that should be enough.
- */
-
-package changelog
